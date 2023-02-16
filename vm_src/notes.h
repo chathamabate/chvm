d0b94abd074517a51efe9eb97c8ae8cf285b38ac
chathamabate <chathamabate@gmail.com>
2023-02-15 19:20:00 -0800

    More thought needed on this one

--- vm_src/notes.h
+++ vm_src/notes.h
@@ -10,5 +10,47 @@
 // Because of how the garbage collector works, we cannot
 // hold an object lock for an indefinite amount of time.
 //
+// The concurrent garbage collector has laid most of the 
+// ground work for allowing user spawned threads.
+//
+// Data Types and Organization :
+//
+// The chvm will respect the following data types...
+//
+// * byte (character)               : byte
+// * signed 64-bit integer          : integer
+// * 64-bit floating point number   : fraction 
+// * Virtual Address    (16-byte)   : virtual
+// * Physical Address   (8-byte)    : physical
+//
+//
+// 
+// A user spawned thread will be given the following resources :
+//
+// * A Data Array useable for arbitrary arithmetic. 
+// * A Table with reference slots. Each slot will hold a virtual
+// address and a physical address. 
+//
+// * A Stack to be used as specified by the user? (Should function calling be built in?)
+// (I think yes tbh)
+//
+// Procedure Call Protocal :
+// 
+// The chvm will have a notion of procedures. It will handle all
+// book-keeping to make calling procedures and returning from procedures possible.
+//
+// The memory given to a procedure need not be in the collected space.
+// As its lifespan is definite. Additionally, its data is never shared
+// with other threads.
+//
+// A user defined procedure will specify how much space it needs for local
+// variables. When calling the procedure said stack frame will be allocated
+// outside of the collected space.
+//
+// How will arguments be passed?
+//
+// 
+// 
+//
 //
 //
